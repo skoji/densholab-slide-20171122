3ee5b912458eaac41b97384ce18c0b26d63dc76e
Satoshi Kojima <skoji@mac.com>
2017-11-19 09:39:23 +0900

    rename class takahashi to t

--- slide-nora-transform.rb
+++ slide-nora-transform.rb
@@ -1,5 +1,5 @@
   modify({:type => :HeadedSection}) do
     @node.classes ||= []
     @node.classes << 'page' if @node.level == 1
-    @node.classes << 'takahashi' if @node.params[0]&.text == 'takahashi'
+    @node.classes << 't' if @node.params[0]&.text == 't'
   end
