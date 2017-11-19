e7a34356828b1fa7454ae28ea48c719df9fa916d
Satoshi Kojima <skoji@mac.com>
2017-11-19 15:52:28 +0900

    add license and some minor modification

--- slide-nora-transform.rb
+++ slide-nora-transform.rb
@@ -3,5 +3,7 @@
     @node.classes << 'page' if @node.level == 1
     @node.classes << 't' if @node.params[0]&.text == 't'
     @node.classes << 'bookguide' if @node.params[0]&.text == 'bookguide'
+    @node.classes << 'license' if @node.params[0]&.text == 'license'
+
 
   end
