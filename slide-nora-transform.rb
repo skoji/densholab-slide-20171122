a4d1d849470dffaafe29e74d3f2f0d7779df2578
Satoshi Kojima <skoji@mac.com>
2017-11-20 08:22:42 +0900

    add target=_blank for all links

--- slide-nora-transform.rb
+++ slide-nora-transform.rb
@@ -4,6 +4,7 @@
     @node.classes << 't' if @node.params[0]&.text == 't'
     @node.classes << 'bookguide' if @node.params[0]&.text == 'bookguide'
     @node.classes << 'license' if @node.params[0]&.text == 'license'
-
-
+  end
+  modify 'l' do
+    @node.add_attr 'target' => ['_blank']
   end
