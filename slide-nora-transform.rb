727f2ebd599e455bd81f68f6bcb8e4995bd20330
Satoshi Kojima <skoji@mac.com>
2017-11-19 14:58:02 +0900

    だいたい流れを書いた

--- slide-nora-transform.rb
+++ slide-nora-transform.rb
@@ -2,4 +2,6 @@
     @node.classes ||= []
     @node.classes << 'page' if @node.level == 1
     @node.classes << 't' if @node.params[0]&.text == 't'
+    @node.classes << 'bookguide' if @node.params[0]&.text == 'bookguide'
+
   end
